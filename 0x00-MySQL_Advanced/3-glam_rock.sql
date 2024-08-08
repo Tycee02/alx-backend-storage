8d496f4ae7243a83777cfd907efe4d42e3df3c02
Tycee02 <wakilaminat@gmail.com>
2024-08-08 15:42:55 +0100

    deleted file

--- 0x00-MySQL_Advanced/3-glam_rock.sql
+++
@@ -1,5 +0,0 @@
--- lists all bands with Glam rock as their main style, ranked by their longevity
-SELECT band_name, (IFNULL(split, '2020')  - formed) AS lifespan
-FROM meta_bands
-WHERE FIND_IN_SET('Glam rock', IFNULL(style, "")) > 0
-ORDER BY lifespan DESC;
