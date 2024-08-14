b45092ba44cb26383a9868d08dcdb4c1526ba3c3
Tycee02 <wakilaminat@gmail.com>
2024-08-14 19:27:52 +0100

    creating get method

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -19,8 +19,6 @@ class Cache:
         self._redis = redis.Redis()
         self._redis.flushdb(True)
 
-    @call_history
-    @count_calls
     def store(self, data: Union[str, bytes, int, float]) -> str:
         """
         store the input data in Redis using the random key and return the key.
