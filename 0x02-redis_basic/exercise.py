cdc1cf5ea46e7f923427de88685b1c19c474fc15
Tycee02 <wakilaminat@gmail.com>
2024-08-14 19:44:12 +0100

    Incrementing values

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -9,6 +9,19 @@ from functools import wraps
 from typing import Any, Callable, Union
 
 
+ddef count_calls(method: Callable) -> Callable:
+    '''Tracks the number of calls made to a method in a Cache class.
+    '''
+    @wraps(method)
+    def invoker(self, *args, **kwargs) -> Any:
+        '''Invokes the given method after incrementing its call counter.
+        '''
+        if isinstance(self._redis, redis.Redis):
+            self._redis.incr(method.__qualname__)
+        return method(self, *args, **kwargs)
+    return invoke
+
+
 class Cache:
     """an object for storing data in a Redis data storage"""
     def __init__(self) -> None:
